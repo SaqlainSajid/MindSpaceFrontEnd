54bd7c9f8ca4b09daaecab846f31150689a8c13e
Saqlain Sajid <ssajid@ualberta.ca>
2023-07-13 23:02:55 -0600

    Splash Screen Works

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text} from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import * as Font from 'expo-font';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -11,14 +12,43 @@ export default function App() {
   const [appIsLoaded, setAppIsLoaded] = useState(false);
 
   useEffect(() => {
+
+    const prepare = async ()=>{
+      try {
+        await Font.loadAsync({
+          'semiBold': require("./assets/Fonts/Montserrat/static/Montserrat-SemiBold.ttf")
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      finally{
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
     
-  })
+  },[]);
+
+  const onLayout = useCallback(async ()=> {
+    if (appIsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if(!appIsLoaded){
+    return null;
+  }
 
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider 
+    style={styles.container}
+    onLayout={onLayout}>
       <SafeAreaView>
-        <Text>Welcome to MindSpace</Text>
+
+        <Text style={styles.text}>Welcome to MindSpace</Text>
         <StatusBar style="auto" />
+
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -28,5 +58,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center"
   },
+  text: {
+    fontFamily: "semiBold",
+  }
 });
