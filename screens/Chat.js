1b1c64286f9d351936aea84b90bd77c5aacf288e
Saqlain Sajid <ssajid@ualberta.ca>
2023-07-15 15:58:30 -0600

    Working on the chat UI now

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+
 
 const Chat = props => {
     return (
-        <View>
-            <Text>Welcome to Chat Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Welcome to Chat Screen</Text>
+            <View>
+                <Button title='image'/>
+                <TextInput/>
+                <Button title='camera'/>
+            </View>
         </View>
+        
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: '#4464bc',
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    text: {
+      fontFamily: "semiBold",
+      color: "#ffffff",
+    }
+  });
 
 export default Chat;
