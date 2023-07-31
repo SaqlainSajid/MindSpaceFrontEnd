952315a84fa75bc5a6ddff0389afb83919ecf1e5
Saqlain Sajid <ssajid@ualberta.ca>
2023-07-31 17:44:47 -0600

    figuring out how to use safe area

--- screens/Auth.js
+++ screens/Auth.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Auth = props => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Give me your personal details or else</Text>
-        </View>
+        </SafeAreaView>
     );
 }
 
