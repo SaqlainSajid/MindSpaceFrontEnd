6d89f84357b9515acdd6b9c540ba2a9a2127e8ae
Saqlain Sajid <ssajid@ualberta.ca>
2023-08-25 22:44:54 -0600

    Restarting Project

--- App.js
+++ App.js
@@ -1,57 +1,12 @@
-import 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback, useEffect, useState } from 'react';
-import * as Font from 'expo-font';
-import AppNavigator from './Navigation/AppNavigator';
-
-
-
-
-SplashScreen.preventAutoHideAsync();
-
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-  const [appIsLoaded, setAppIsLoaded] = useState(false);
-
-  useEffect(() => {
-
-    const prepare = async ()=>{
-      try {
-        await Font.loadAsync({
-          'semiBold': require("./assets/Fonts/Montserrat/static/Montserrat-SemiBold.ttf")
-        });
-      } catch (error) {
-        console.log(error);
-      }
-      finally{
-        setAppIsLoaded(true);
-      }
-    };
-
-    prepare();
-    
-  },[]);
-
-  const onLayout = useCallback(async ()=> {
-    if (appIsLoaded){
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsLoaded]);
-
-  if(!appIsLoaded){
-    return null;
-  }
-
   return (
-    <SafeAreaProvider 
-    style={styles.container}
-    onLayout={onLayout}>
-      <AppNavigator/>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
@@ -59,8 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
-    fontFamily: "semiBold",
-  }
 });
