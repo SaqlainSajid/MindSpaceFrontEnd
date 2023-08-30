7e9317b6dd392382c0ba19fd5eff835fc23591e2
Saqlain Sajid <ssajid@ualberta.ca>
2023-08-29 21:31:57 -0600

    Built login screen without Form Validation

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { StyleSheet, View, Text, SafeAreaView, Platform, StatusBar } from 'react
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import SignUpScreen from './app/screens/SignUpScreen';
+import LoginScreen from './app/screens/LoginScreen';
 
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
-        {/* <WelcomeScreen/> */}
-        <SignUpScreen/>
+        <LoginScreen/>
       </View>
     </SafeAreaProvider>
   );
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,143 @@
+import React from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  StatusBar,
+  useColorScheme,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import Button from "../components/Button";
+
+export default function SignUpScreen() {
+  const theme = useColorScheme();
+
+  return (
+    <SafeAreaView edges={["top", "right", "left"]} style={styles.container}>
+      <LinearGradient
+        style={styles.gradient}
+        colors={[
+          "#dedae6",
+          "#cfbede",
+          "#8381bd",
+          "#8381bd",
+          "#cfbede",
+          "#dedae6",
+        ]}
+        locations={[0, 0.1, 0.3, 0.7, 0.9, 1]}
+      >
+        <View style={styles.mainTextContainer}>
+          <Text style={[styles.text, styles.mainText]}>Welcome</Text>
+          <Text style={[styles.text, styles.mainText]}>Back</Text>
+          <View style={styles.secondaryTextContainer}>
+            <Text style={[styles.text, styles.secondaryText]}>
+              Log in to continue
+            </Text>
+          </View>
+        </View>
+        <View style={styles.form}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.text}>EMAIL/USERNAME</Text>
+            <TextInput style={styles.input}></TextInput>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.text}>PASSWORD</Text>
+            <TextInput style={styles.input} secureTextEntry={true}></TextInput>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              style={styles.button}
+              text="Login"
+              class="primary"
+              Style={{ width: 300 }}
+            />
+          </View>
+          <View style={styles.FooterTextContainer}>
+            <TouchableOpacity>
+              <Text style={[styles.text, styles.FooterText]}>
+                Forgot Password?
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </LinearGradient>
+      <StatusBar
+        barStyle={theme === "dark" ? "dark-content" : "light-content"}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0A1145",
+    width: Dimensions.get("screen").width,
+    justifyContent: "space-evenly",
+  },
+  gradient: {
+    flex: 1,
+  },
+  mainTextContainer: {
+    flex: 0.3,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  mainText: {
+    fontSize: 32,
+    lineHeight: 32,
+    fontWeight: "bold",
+  },
+  secondaryTextContainer: {
+    flexDirection: "row",
+  },
+  secondaryText: {
+    fontSize: 14,
+  },
+  linkText: {
+    textDecorationLine: "underline",
+    fontSize: 14,
+  },
+  form: {
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    paddingTop: 20,
+    marginHorizontal: 50,
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: "space-around",
+  },
+  input: {
+    width: 300,
+    height: 60,
+    backgroundColor: "#c5c6e6",
+    borderRadius: 20,
+    fontSize: 18,
+    paddingHorizontal: 16,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "space-evenly",
+  },
+  button: {
+    alignSelf: "center",
+  },
+  FooterTextContainer: {
+    flex: 0.5,
+    justifyContent: "flex-start",
+  },
+  FooterText: {
+    textDecorationLine: "underline",
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+  },
+});
