d2eb9f7213973e1611b24ec5d0fa9fdd8af91c57
Saqlain Sajid <ssajid@ualberta.ca>
2023-08-31 04:14:15 -0600

    cleaning up useless imports

--- app/screens/MySpace/MySpace.js
+++ app/screens/MySpace/MySpace.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenTemplate from "../../components/ScreenTemplate";
-import NavBar from "../../components/NavBar";
 
 const MySpace = () => {
   return (
