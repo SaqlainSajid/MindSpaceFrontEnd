002d657ebad3c680115f2368ad3a710721655427
Saqlain Sajid <ssajid@ualberta.ca>
2023-09-03 16:54:07 -0600

    started making Relaxation

--- app/screens/Relaxation/Relaxation.js
+++ app/screens/Relaxation/Relaxation.js
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import ScreenTemplate from "../../components/ScreenTemplate";
 
 const Relaxation = () => {
   return (
     <ScreenTemplate>
-      <View>
-        <Text>Relaxation</Text>
+      <View style={styles.headerView}>
+        <Text style={styles.mainText}>How are you feeling today?</Text>
+        <Text style={styles.secondaryText}>
+          Tell us and we'll suggest a sound!
+        </Text>
+      </View>
+      <View style={styles.cardGrid}></View>
+      <View style={styles.showMore}>
+        <TouchableOpacity>
+          <Text style={styles.showMoreText}>Show More</Text>
+        </TouchableOpacity>
       </View>
     </ScreenTemplate>
   );
@@ -14,4 +23,34 @@ const Relaxation = () => {
 
 export default Relaxation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: "space-around",
+  },
+  headerView: {
+    flex: 0.2,
+    backgroundColor: "#f2eff2",
+    borderRadius: 20,
+    margin: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mainText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  secondaryText: {
+    marginTop: 10,
+  },
+  cardGrid: {
+    flex: 1,
+  },
+  showMore: {
+    flex: 0.15,
+    alignItems: "center",
+  },
+  showMoreText: {
+    color: "white",
+  },
+});
