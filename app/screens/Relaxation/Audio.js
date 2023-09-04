505653ea7a0864f0a8d1db9db4eca27ea6082989
Saqlain Sajid <ssajid@ualberta.ca>
2023-09-04 17:40:28 -0600

    Delete CardGrid & Row, make audio screen

---
+++ app/screens/Relaxation/Audio.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenTemplate from "../../components/ScreenTemplate";
+
+const Audio = () => {
+  return (
+    <ScreenTemplate>
+      <View>
+        <Text>Audio</Text>
+      </View>
+    </ScreenTemplate>
+  );
+};
+
+export default Audio;
+
+const styles = StyleSheet.create({});
