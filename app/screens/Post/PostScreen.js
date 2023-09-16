06ab9dc298b97a64b18518b3dc21c77297a5671a
Saqlain Sajid <ssajid@ualberta.ca>
2023-09-16 01:45:17 -0600

    Made the header for the post

--- app/screens/Post/PostScreen.js
+++ app/screens/Post/PostScreen.js
@@ -7,12 +7,22 @@ const PostScreen = ({ route }) => {
   return (
     <ScreenTemplate>
       <View style={styles.container}>
-        <Image style={styles.image} source={passingValues.userpic} />
-        <Text>{passingValues.username}</Text>
-        <Text>{passingValues.content}</Text>
-        <Text>{passingValues.time}</Text>
-        <Text>heart: {passingValues.reactions.heart}</Text>
-        <Text>comment: {passingValues.reactions.heart}</Text>
+        <View style={styles.header}>
+          <View style={styles.profile}>
+            <Image style={styles.image} source={passingValues.userpic} />
+            <Text style={styles.username}>{passingValues.username}</Text>
+          </View>
+          <Text style={{ fontSize: 14, fontWeight: "300" }}>
+            {passingValues.time}
+          </Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={{ fontSize: 18, fontWeight: "400" }}>
+            {passingValues.content}
+          </Text>
+          <Text>heart: {passingValues.reactions.heart}</Text>
+          <Text>comment: {passingValues.reactions.heart}</Text>
+        </View>
         <Text>
           {passingValues.comments[0].username}:
           {passingValues.comments[0].content}
@@ -43,5 +53,25 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20,
+  },
+  header: {
+    marginTop: 10,
+    marginHorizontal: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  profile: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  username: {
+    fontSize: 24,
+    fontWeight: "300",
+    marginLeft: 10,
+  },
+  content: {
+    flex: 0.2,
+    justifyContent: "center",
   },
 });
