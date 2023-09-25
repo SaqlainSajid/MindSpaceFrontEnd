481c25e2bb2a96a4a024ad9b1764507c4ae539db
Saqlain Sajid <ssajid@ualberta.ca>
2023-09-25 15:19:11 -0600

    started making the buttons

--- app/screens/BookSession/Booking.js
+++ app/screens/BookSession/Booking.js
@@ -6,35 +6,41 @@ const Booking = () => {
   return (
     <ScreenTemplate>
       <View style={styles.container}>
-        <Text>Duration</Text>
-        <TouchableOpacity>
-          <Text>60 minutes</Text>
+        <Text style={styles.header}>Duration</Text>
+        <TouchableOpacity style={styles.buttoncontainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text>60 minutes</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>90 minutes</Text>
+          </TouchableOpacity>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>90 minutes</Text>
+        <Text style={styles.header}>Time</Text>
+        <TouchableOpacity style={styles.buttoncontainer}>
+          <TouchableOpacity style={[styles.button, styles.timebutton]}>
+            <Text>11:00 am</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.timebutton]}>
+            <Text>12:00 pm</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.timebutton]}>
+            <Text>1:00 pm</Text>
+          </TouchableOpacity>
         </TouchableOpacity>
-        <Text>Time</Text>
-        <TouchableOpacity>
-          <Text>11:00 am</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>12:00 pm</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>1:00 pm</Text>
-        </TouchableOpacity>
-        <Text>Date</Text>
+        <Text style={styles.header}>Date</Text>
         <View>
           <Text>Calendar</Text>
         </View>
-        <Text>Preferrable mode of confirmation</Text>
-        <TouchableOpacity>
-          <Text>Call</Text>
+        <Text style={styles.header}>Preferred mode of confirmation</Text>
+        <TouchableOpacity style={styles.buttoncontainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text>Call</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>Email</Text>
+          </TouchableOpacity>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Email</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
           <Text>Pay Advance</Text>
         </TouchableOpacity>
       </View>
@@ -48,5 +54,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+    margin: 10,
+    padding: 15,
+    borderRadius: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 10,
+  },
+  buttoncontainer: {
+    marginVertical: 30,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    overflow: "scroll",
+  },
+  button: {
+    // backgroundColor: "#292c52",
+    borderWidth: 2,
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 30,
+  },
+  timebutton: {
+    marginHorizontal: 30,
   },
 });
