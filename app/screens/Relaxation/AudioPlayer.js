f8a037273d2f58ce6f297a05d6ed24abbc7fe1ae
Saqlain Sajid <ssajid@ualberta.ca>
2023-11-16 18:10:36 -0600

    audio plays, pauses but doesn't stop when leaving the screen

--- app/screens/Relaxation/AudioPlayer.js
+++ app/screens/Relaxation/AudioPlayer.js
@@ -1,19 +1,61 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
 import { Audio } from "expo-av";
 import { Ionicons } from "react-native-vector-icons";
 import ScreenTemplate from "../../components/ScreenTemplate";
 
 const AudioPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [sound, setSound] = useState(new Audio.Sound());
+
+  useEffect(() => {
+    return sound.current
+      ? () => {
+          sound.current.unloadAsync();
+        }
+      : undefined;
+  }, []);
+
+  const handlePlayPause = async () => {
+    if (!isPlaying) {
+      const { sound: playbackObject } = await Audio.Sound.createAsync(
+        require("../../assets/paris.mp3"),
+        { shouldPlay: true }
+      );
+      sound.current = playbackObject;
+      setIsPlaying(true);
+    } else {
+      await sound.current.pauseAsync();
+      setIsPlaying(false);
+    }
+  };
+
   return (
     <ScreenTemplate>
       <View style={styles.main}>
-        <Text>Audio</Text>
-        <TouchableOpacity
-          style={styles.play}
-          // onPress={this.loadAudio}
+        <View style={styles.picview}>
+          <Image
+            style={styles.image}
+            source={require("../../assets/mountain.jpg")}
+          ></Image>
+        </View>
+        <Text
+          style={{
+            alignSelf: "center",
+            marginTop: 10,
+            fontWeight: "500",
+            fontSize: 20,
+          }}
         >
-          <Ionicons name="play" size={35} style={{ color: "white" }} />
+          {" "}
+          Best Audio In The Business{" "}
+        </Text>
+        <TouchableOpacity style={styles.playview} onPress={handlePlayPause}>
+          <Ionicons
+            name={isPlaying ? "pause" : "play"}
+            size={20}
+            style={{ color: "white", marginLeft: 3 }}
+          />
         </TouchableOpacity>
       </View>
     </ScreenTemplate>
@@ -30,14 +72,33 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 25,
   },
-  play: {
+  playview: {
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 60,
-    height: 80,
-    width: 80,
-    backgroundColor: "red",
+    borderRadius: 40,
+    height: 45,
+    width: 45,
+    backgroundColor: "#8772a3",
     alignSelf: "center",
     marginBottom: 20,
+    marginTop: 20,
+  },
+  picview: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 2,
+    padding: 10,
+    borderRadius: 25,
+    borderWidth: 5,
+    marginBottom: 10,
+    borderColor: "#8772a3",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+    borderWidth: 4,
+    borderColor: "#8772a3",
   },
 });
