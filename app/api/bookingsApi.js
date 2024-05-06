9f4464ed356018dc9bacaa164b7fdb3f85e7135d
ssajid <ssajid@ualberta.ca>
2024-05-06 15:01:33 -0600

    booking api for confirmed, previous and pending user bookings

--- app/api/bookingsApi.js
+++ app/api/bookingsApi.js
@@ -6,7 +6,19 @@ const getDocDateBookings = (docId, date) =>
   apiClient.get(`${endpoint1}/doc/${docId}/date/${date}`);
 
 const setBooking = (booking) => apiClient.post(endpoint1, booking);
+const getUserPendingBookings = (userId) =>
+  apiClient.get(`${endpoint1}/pending/${userId}`);
+const getUserConfirmedBookings = (userId) =>
+  apiClient.get(`${endpoint1}/confirmed/${userId}`);
+const getUserPastBookings = (userId) =>
+  apiClient.get(`${endpoint1}/previous/${userId}`);
 
-export default { getDocDateBookings, setBooking };
+export default {
+  getDocDateBookings,
+  setBooking,
+  getUserPendingBookings,
+  getUserConfirmedBookings,
+  getUserPastBookings,
+};
 
 //adds times to availabilities of a doctor
